3f7c13a2913d68e8dcb483d6f10a2f43bf3bed30
Yan Min <yc89@hw.ac.uk>
2024-11-22 16:20:00 +0000

    Finished the design of GUI

--- View.py
+++ View.py
@@ -33,8 +33,8 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title("GUI")
-        self.geometry("600x400+50+50")
+        self.title("Home")
+        self.geometry("340x200")
 
 
         container = tk.Frame(self)
@@ -48,66 +48,168 @@ class Window(tk.Tk):
         menu_i = tk.Menu(menu, tearoff=0)
         menu.add_cascade(label="Features", menu=menu_i)
 
-        for f in (BlankPage, ViewByCountryPage, ViewByContinentPage):
+        for f in (BlankPage, ViewByCountryPage, ViewByContinentPage, ViewByBrowser, ViewByMainBrowser, ReaderProfiles, AlsoLikes):
             self.frames[f] = f(container, self)
             self.frames[f].grid(row=0, column=0, stick="nsew")
             menu_i.add_command(label=self.frames[f].page_name, command=self.frames[f].go_to)
 
-        self.select_frame(BlankPage)
+        self.select_frame(ViewByCountryPage)
         self.config(menu=menu)
 
     def select_frame(self, frame):
         self.frames[frame].tkraise()
 
-class BlankPage(tk.Frame):
-
+class PopUp(tk.Frame):
     def __init__(self, parent, window):
         self.window = window
         super().__init__(parent)
+
+    def go_to(self):
+        self.window.select_frame(type(self))
+        self.window.title(self.page_name)  
+
+# The Home page (blank)
+class BlankPage(PopUp):
+
+    def __init__(self, parent, window):
+        super().__init__(parent, window)
         self.page_name = "Home"
 
     def go_to(self):
-        self.window.select_frame(type(self))
+        super().go_to()
 
-class ViewByCountryPage(tk.Frame):
+# The view by country page
+class ViewByCountryPage(PopUp):
     
     def __init__(self, parent, window):
-        self.window = window
-        super().__init__(parent)
+        super().__init__(parent, window)
         self.page_name = "View by Country"
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
 
         # The input boxes
         label = tk.Label(self, text = "Document UUID")
-        label.pack()
+        label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
         self.docID = tk.StringVar() # store docID value
-        self.docID_entry = tk.Entry(self, textvariable=self.docID)
-        self.docID_entry.pack(fill = 'x', expand = True, padx = 10)
+        docID_entry = tk.Entry(self, textvariable=self.docID)
+        docID_entry.grid(row=0, column=1, sticky=tk.EW, padx=5, pady=5)
 
         # The buttons
-        self.view_by_country_button = tk.Button(self, text="Ok", command = "view_by_country_clicked")
-        self.view_by_country_button.pack(fill='x', expand = True, pady = 10, padx = 10)
+        view_by_country_button = tk.Button(self, text="Ok", command = "view_by_country_clicked")
+        view_by_country_button.grid(row=1, column=1, ipadx=10, pady=5)
 
     def go_to(self):
-        self.window.select_frame(type(self))
+        super().go_to()
 
-class ViewByContinentPage(tk.Frame):
+# The view by continent page
+class ViewByContinentPage(PopUp):
 
     def __init__(self, parent, window):
-        self.window = window
-        super().__init__(parent)
+        super().__init__(parent, window)
         self.page_name = "View by Continent"
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
 
         # The input boxes
+        label = tk.Label(self, text = "Document UUID")
+        label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
         self.docID = tk.StringVar() # store docID value
-        self.docID_entry = tk.Entry(self, textvariable=self.docID)
-        self.docID_entry.pack(fill = 'x', expand = True)
+        docID_entry = tk.Entry(self, textvariable=self.docID)
+        docID_entry.grid(row=0, column=1, sticky=tk.EW, padx=5, pady=5)
 
         # The buttons
-        self.view_by_continent_button = tk.Button(self, text="Ok (continent)", command = "view_by_continent_clicked")
-        self.view_by_continent_button.pack(fill='x', expand = True, pady = 10)
+        view_by_continent_button = tk.Button(self, text="Ok", command="view_by_continent_clicked")
+        view_by_continent_button.grid(row=1, column=1, ipadx=10, pady=5)
 
     def go_to(self):
-        self.window.select_frame(type(self))
+        super().go_to()
+
+class ViewByBrowser(PopUp):
+
+    def __init__(self, parent, window):
+        super().__init__(parent, window)
+        self.page_name = "View by Browser"
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
+
+        # The menu of event type
+        label = tk.Label(self, text = "Event type")
+        label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+        event_name = tk.StringVar()
+        options = ["a", "b", "c"] #CHANGE THIS ONEEEEEEEE TO A METHOD THAT RECEIVE EVENT TYPE FROM MODEL.PY!!!!!!!!!!!!!
+        event_optionMenu = tk.OptionMenu(self, event_name, *options)
+        event_optionMenu.grid(row=0, column=1, sticky=tk.EW, padx=5, pady=5)
+
+        # The buttons
+        view_by_browser_button = tk.Button(self, text="Ok", command="view_by_browser_clicked")
+        view_by_browser_button.grid(row=1, column=1, ipadx=10, pady=5)
+    
+    def go_to(self):
+        super().go_to()
+
+class ViewByMainBrowser(PopUp):
+
+    def __init__(self, parent, window):
+        super().__init__(parent, window)
+        self.page_name = "View by Main Browser"
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
+
+        # The menu of event type
+        label = tk.Label(self, text = "Event type")
+        label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+        event_name = tk.StringVar()
+        options = ["a", "b", "c"] #CHANGE THIS ONEEEEEEEE TO A METHOD THAT RECEIVE EVENT TYPE FROM MODEL.PY!!!!!!!!!!!!!
+        event_optionMenu = tk.OptionMenu(self, event_name, *options)
+        event_optionMenu.grid(row=0, column=1, sticky=tk.EW, padx=5, pady=5)
+
+        # The buttons
+        view_by_main_browser_button = tk.Button(self, text="Ok", command="view_by_main_browser_clicked")
+        view_by_main_browser_button.grid(row=1, column=1, ipadx=10, pady=5)
+    
+    def go_to(self):
+        super().go_to()
+
+class ReaderProfiles(PopUp):
+
+    def __init__(self, parent, window):
+        super().__init__(parent, window)
+        self.page_name = "Reader Profiles"
+
+        # Add method to load reader profile from Model.py
+
+    def go_to(self):
+        super().go_to()
+
+class AlsoLikes(PopUp):
+
+    def __init__(self, parent, window):
+        super().__init__(parent, window)
+        self.page_name = "Also Likes"
+
+        # GUI column design
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
+
+        # The input boxes
+        label = tk.Label(self, text = "Document UUID")
+        label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+        self.docID = tk.StringVar() # store docID value
+        docID_entry = tk.Entry(self, textvariable=self.docID)
+        docID_entry.grid(row=0, column=1, sticky=tk.EW, padx=5, pady=5)
+
+        # The buttons
+        also_likes_button = tk.Button(self, text="Ok", command="also_likes_clicked")
+        also_likes_button.grid(row=1, column=1, ipadx=10, pady=5)
+        also_likes_generate_graph = tk.Button(self, text="Generate graph", command="also_likes_generate_graph_clicked")
+        also_likes_generate_graph.grid(row=2, column=1, ipadx=10, pady=5)
+
+    def go_to(self):
+        super().go_to()
 
 win = Window()
 win.mainloop()
